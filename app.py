28d99c7a7f8f1c24353e5797750738ef3b639ba2
AB-Yusuf <engryusufmayowa@gmail.com>
2019-10-10 17:38:59 +0100

    Adding parameters to an endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
 
 
 app = Flask(__name__)
@@ -6,13 +7,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!, I am happy'
 
 
-@app.route('/super_simple')
+@app.route('/super')
 def super_simple():
-    return 'This is a planetary api'
+    return jsonify(message='Welcome to Planetary Api')
 
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message='Sorry' + name + ', you are not old enough.'), 401
+    else:
+        return jsonify(message='Welcome ' + name + ', you are old enough.')
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
